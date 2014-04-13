f59b2c6f88e1cfb60b77b4fcf671e6274b6ed07b
akazero2 <akazero2@localhost>
2014-04-13 14:25:09 -0400

    Refactored event handling methods in MyForm.h

--- TeamKTextTwist/MyForm.cpp
+++ TeamKTextTwist/MyForm.cpp
@@ -13,85 +13,32 @@ using namespace controller;
 namespace Project1 {
 
 System::Void MyForm::generateButton_Click(System::Object^  sender, System::EventArgs^  e) {
-	this->lettersBox->Text = gc->getRandomLetters(7);
-	this->shuffleButton->Enabled = true;
+	this->handleGenerateEvent();
 }
 
 System::Void MyForm::shuffleButton_Click(System::Object^  sender, System::EventArgs^  e) {
-	if (this->lettersBox->Text != "") {
-		String^ letters = this->lettersBox->Text;
-		letters = this->gc->shuffleLetters(letters);
-		this->lettersBox->Text = letters;
-	}
+	this->handleShuffle();
 }
 
 System::Void MyForm::nameBox_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-	if (this->nameBox->Text->Length != 0) {
-		this->startButton->Enabled = true;
-	} else {
-		this->startButton->Enabled = false;
-	}
+	this->toggleStartButtonEnabled();
 }
 
 System::Void MyForm::startButton_Click(System::Object^  sender, System::EventArgs^  e) {
-	String^ playerName = this->nameBox->Text;
-	this->gc->setPlayerName(playerName);
-	this->generateButton->Enabled = true;
-	this->submitButton->Enabled = true;
+	this->beginNewGame();
 }
 
 System::Void MyForm::submitButton_Click(System::Object^  sender, System::EventArgs^  e) {
-	if (this->guessBox->Text->Length >= 3) {
-		String^ value = this->guessBox->Text;
-		Word^ newWord = gcnew Word(value);
-		String^ allowedLetters = this->lettersBox->Text;
-		if (this->gc->isWordValid(newWord, allowedLetters)) {
-			int pointValue = newWord->getPointValue();
-			this->guessedWords->AppendText(value + " (" + pointValue + ")" + "\n");
-			this->gc->incrementPlayerScore(pointValue);
-			if (!this->reuseLetters)
-			{
-				String^ newAllowedLetters = this->removeCharacters(value, allowedLetters);
-				this->lettersBox->Text = newAllowedLetters; 
-			}
-			this->scoreLabel->Text = this->gc->getPlayerScoreString();
-		} else {
-			MessageBox::Show("Word not allowed. You have lost one point.");
-			this->gc->decrementPlayerScore();
-			this->scoreLabel->Text = this->gc->getPlayerScoreString();
-		}
-	} else {
-		MessageBox::Show("Your guess must be at least three letters long");
-	}
-	this->guessBox->Text = "";
+	this->submitWord();
 }
 
-String^ MyForm::removeCharacters(String^ ofWord, String^ fromString) {
-	List<char>^ fromList = this->gc->stringToChars(fromString);
-	for (int i = 0; i < ofWord->Length; i++) {
-		fromList->Remove(ofWord[i]);
-	}
-
-	StringBuilder^ builder = gcnew StringBuilder();
-	for (int i = 0; i < fromList->Count; i++) {
-		builder->Append(Char::ToString(fromList[i]));
-	}
-
-	return builder->ToString();
-}	
-
 System::Void MyForm::optionsToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
-	OptionsDialog^ optionsDialog = gcnew OptionsDialog(this->timeLimit, this->reuseLetters);
-
-	if (optionsDialog->ShowDialog() == ::DialogResult::OK) {
-		this->reuseLetters = optionsDialog->getLetterReuse();
-		this->timeLimit = optionsDialog->getTimeLimit();
-	}
+	this->showOptionsMenu();
 }
 
 System::Void MyForm::exitToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
 			 Form::Close();
-		 }
+}
 
 System::Void MyForm::newGameToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
 	this->startNewGame();
@@ -110,6 +57,14 @@ System::Void MyForm::newGameButton_Click(System::Object^ sender, System::EventAr
 	this->startNewGame();
 }
 
+System::Void MyForm::lettersBox_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->newGameButton->Focus();
+}
+
+System::Void MyForm::guessedWords_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->newGameButton->Focus();
+}
+
 void MyForm::startNewGame() {
 	this->nameBox->Text = "";
 	this->lettersBox->Text = "";
@@ -119,5 +74,90 @@ void MyForm::startNewGame() {
 	this->gc->createNewPlayer();
 	this->scoreLabel->Text = this->gc->getPlayerScoreString();
 	this->guessedWords->Text = "";
+}
+
+void MyForm::performIfReuseIsOff(String^ value, String^ allowedLetters) {
+	if (!this->reuseLetters)
+	{
+			String^ newAllowedLetters = this->removeCharacters(value, allowedLetters);
+			this->lettersBox->Text = newAllowedLetters; 
+	}
+}
+
+void MyForm::handleWordEntry() {
+	String^ value = this->guessBox->Text;
+	Word^ newWord = gcnew Word(value);
+	String^ allowedLetters = this->lettersBox->Text;
+	if (this->gc->isWordValid(newWord, allowedLetters)) {
+		int pointValue = newWord->getPointValue();
+		this->guessedWords->AppendText(value + " (" + pointValue + ")" + "\n");
+		this->gc->incrementPlayerScore(pointValue);
+		this->performIfReuseIsOff(value, allowedLetters);
+		this->scoreLabel->Text = this->gc->getPlayerScoreString();
+	} else {
+		MessageBox::Show("Word not allowed. You have lost one point.");
+		this->gc->decrementPlayerScore();
+		this->scoreLabel->Text = this->gc->getPlayerScoreString();
+	}
+}
+
+void MyForm::submitWord() {
+	if (this->guessBox->Text->Length >= 3) {
+		this->handleWordEntry();
+	} else {
+		MessageBox::Show("Your guess must be at least three letters long");
+	}
+	this->guessBox->Text = "";
+}
+
+void MyForm::handleGenerateEvent() {
+	this->lettersBox->Text = gc->getRandomLetters(7);
+	this->shuffleButton->Enabled = true;
+}
+
+void MyForm::handleShuffle() {
+	if (this->lettersBox->Text != "") {
+		String^ letters = this->lettersBox->Text;
+		letters = this->gc->shuffleLetters(letters);
+		this->lettersBox->Text = letters;
+	}
+}
+
+void MyForm::toggleStartButtonEnabled() {
+	if (this->nameBox->Text->Length != 0) {
+		this->startButton->Enabled = true;
+	} else {
+		this->startButton->Enabled = false;
+	}
+}
+
+void MyForm::beginNewGame() {
+	String^ playerName = this->nameBox->Text;
+	this->gc->setPlayerName(playerName);
+	this->generateButton->Enabled = true;
+	this->submitButton->Enabled = true;
+}
+
+String^ MyForm::removeCharacters(String^ ofWord, String^ fromString) {
+	List<char>^ fromList = this->gc->stringToChars(fromString);
+	for (int i = 0; i < ofWord->Length; i++) {
+		fromList->Remove(ofWord[i]);
+	}
+
+	StringBuilder^ builder = gcnew StringBuilder();
+	for (int i = 0; i < fromList->Count; i++) {
+		builder->Append(Char::ToString(fromList[i]));
+	}
+
+	return builder->ToString();
+}
+
+void MyForm::showOptionsMenu() {
+	OptionsDialog^ optionsDialog = gcnew OptionsDialog(this->timeLimit, this->reuseLetters);
+
+	if (optionsDialog->ShowDialog() == ::DialogResult::OK) {
+		this->reuseLetters = optionsDialog->getLetterReuse();
+		this->timeLimit = optionsDialog->getTimeLimit();
+	}
 }
 }
--- TeamKTextTwist/MyForm.h
+++ TeamKTextTwist/MyForm.h
@@ -23,6 +23,7 @@ namespace Project1 {
 			InitializeComponent();
 			this->timeLimit = DEFAULT_TIME_LIMIT;
 			this->reuseLetters = false;
+			this->newGameButton->Focus();
 			this->gc = gcnew GameController();
 		}
 
@@ -53,7 +54,8 @@ namespace Project1 {
 	private: System::Windows::Forms::TextBox^  nameBox;
 	private: System::Windows::Forms::Button^  startButton;
 	private: System::Windows::Forms::Label^  scoreLabel;
-	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::MenuStrip^  menuBar;
+
 	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  newGameToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  highScoresToolStripMenuItem;
@@ -74,12 +76,22 @@ namespace Project1 {
 	private: System::Void submitButton_Click(System::Object^  sender, System::EventArgs^  e);
 	private: String^ removeCharacters(String^ ofWord, String^ fromString);
 	private: void startNewGame();
+	private: void performIfReuseIsOff(String^ value, String^ allowedLetters);
+	private: void handleWordEntry();
+	private: void submitWord();
+	private: void handleGenerateEvent();
+	private: void handleShuffle();
+	private: void toggleStartButtonEnabled();
+	private: void beginNewGame();
+	private: void showOptionsMenu();
 	private: System::Void optionsToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
 	private: System::Void exitToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
 	private: System::Void newGameToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
 	private: System::Void highScoresToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
 	private: System::Void clearAllButton_Click(System::Object^  sender, System::EventArgs^  e);
 	private: System::Void newGameButton_Click(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void lettersBox_Click(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void guessedWords_Click(System::Object^  sender, System::EventArgs^  e);
 
 	private:
 		/// <summary>
@@ -106,7 +118,7 @@ namespace Project1 {
 			this->nameBox = (gcnew System::Windows::Forms::TextBox());
 			this->startButton = (gcnew System::Windows::Forms::Button());
 			this->scoreLabel = (gcnew System::Windows::Forms::Label());
-			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->menuBar = (gcnew System::Windows::Forms::MenuStrip());
 			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->newGameToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->highScoresToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
@@ -114,7 +126,7 @@ namespace Project1 {
 			this->exitToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->clearAllButton = (gcnew System::Windows::Forms::Button());
 			this->newGameButton = (gcnew System::Windows::Forms::Button());
-			this->menuStrip1->SuspendLayout();
+			this->menuBar->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// generateButton
@@ -173,16 +185,20 @@ namespace Project1 {
 			this->lettersBox->Size = System::Drawing::Size(268, 55);
 			this->lettersBox->TabIndex = 2;
 			this->lettersBox->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->lettersBox->Click += gcnew System::EventHandler(this, &MyForm::lettersBox_Click);
 			// 
 			// guessedWords
 			// 
+			this->guessedWords->Cursor = System::Windows::Forms::Cursors::Default;
 			this->guessedWords->Location = System::Drawing::Point(309, 172);
 			this->guessedWords->Margin = System::Windows::Forms::Padding(2);
 			this->guessedWords->Multiline = true;
 			this->guessedWords->Name = L"guessedWords";
 			this->guessedWords->ReadOnly = true;
+			this->guessedWords->ScrollBars = System::Windows::Forms::ScrollBars::Horizontal;
 			this->guessedWords->Size = System::Drawing::Size(268, 151);
 			this->guessedWords->TabIndex = 5;
+			this->guessedWords->Click += gcnew System::EventHandler(this, &MyForm::guessedWords_Click);
 			// 
 			// label1
 			// 
@@ -253,15 +269,15 @@ namespace Project1 {
 			this->scoreLabel->TabIndex = 11;
 			this->scoreLabel->Text = L"Score: 0";
 			// 
-			// menuStrip1
+			// menuBar
 			// 
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->fileToolStripMenuItem});
-			this->menuStrip1->Location = System::Drawing::Point(0, 0);
-			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Padding = System::Windows::Forms::Padding(4, 1, 0, 1);
-			this->menuStrip1->Size = System::Drawing::Size(615, 24);
-			this->menuStrip1->TabIndex = 12;
-			this->menuStrip1->Text = L"menuStrip1";
+			this->menuBar->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->fileToolStripMenuItem});
+			this->menuBar->Location = System::Drawing::Point(0, 0);
+			this->menuBar->Name = L"menuBar";
+			this->menuBar->Padding = System::Windows::Forms::Padding(4, 1, 0, 1);
+			this->menuBar->Size = System::Drawing::Size(615, 24);
+			this->menuBar->TabIndex = 12;
+			this->menuBar->Text = L"menuStrip1";
 			// 
 			// fileToolStripMenuItem
 			// 
@@ -342,17 +358,19 @@ namespace Project1 {
 			this->Controls->Add(this->lettersBox);
 			this->Controls->Add(this->lettersLabel);
 			this->Controls->Add(this->generateButton);
-			this->Controls->Add(this->menuStrip1);
-			this->MainMenuStrip = this->menuStrip1;
+			this->Controls->Add(this->menuBar);
+			this->MainMenuStrip = this->menuBar;
 			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
+			this->ShowIcon = false;
 			this->Text = L"Text Twist by Ayaan Kazerouni and Joe Culberson";
-			this->menuStrip1->ResumeLayout(false);
-			this->menuStrip1->PerformLayout();
+			this->menuBar->ResumeLayout(false);
+			this->menuBar->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
+
 };
 }
